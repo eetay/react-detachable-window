13387bcae931e63488bcceaa6fcd36e666ccfd21
Eetay Natan <eetay.natan@samanage.com>
2018-07-23 13:45:16 +0300

    linting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class ReactDetachableWindow extends React.PureComponent {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.containerEl = document.createElement('div')
     this.externalWindow = null
@@ -32,27 +32,33 @@ export default class ReactDetachableWindow extends React.PureComponent {
     this.setState({ portal: true })
   }
 
-  render () {
-    console.log('RENDER', this.portal, this.state.showWindowPortal)
+  render() {
+    // console.log('RENDER', this.portal, this.state.showWindowPortal)
     return this.state.portal ? ReactDOM.createPortal(
       <div>
         {this.props.children}
-        <button onClick={this.closeWindow}>
+        <button type='button' onClick={this.closeWindow}>
           Close me!
         </button>
       </div>,
       this.containerEl
-    ) : (<div>{this.props.children}<button onClick={this.openWindow}>Open in popup!</button></div>)
+    ) : (
+      <div>
+        {this.props.children}
+        <button type='button' onClick={this.openWindow}>
+          Open in popup!
+        </button>
+      </div>
+    )
   }
 
-  componentDidMount () {
-    console.log('componentDidMount')
+  componentDidMount() {
+    // console.log('componentDidMount')
     this.closeWindow() // initial state of window when widget is launched
   }
 
-  componentWillUnmount () {
-    console.log('componentWillUnmount')
+  componentWillUnmount() {
+    // console.log('componentWillUnmount')
     this.closeWindow() // close window when unmounting
   }
 }
-
